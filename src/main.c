f4e234aa1152a7336d7d40117ab41519325c9c7d
LumingSun <sunluming@ruc.edu.cn>
2019-10-31 23:29:41 +0800

    add resize

--- src/main.c
+++ src/main.c
@@ -2,9 +2,13 @@
 // Created by luming on 2019/10/28.
 //
 #include "hash_table.h"
-
-
+#include "prime.h"
+#include <string.h>
+#include <stdio.h>
 int main() {
     ht_hash_table* ht = ht_new();
+    ht_insert(ht,"a","aa");
+    ht_insert(ht,"b","bb");
+//    ht_search(ht,"bb");
     ht_del_hash_table(ht);
 }
